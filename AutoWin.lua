10e0c07214ca131da2a52775a5f169ce74b0d3b6
fjjjjjjjjjjjjjjjjjjjjqwdqugdqugqdqnqsiqugdqdqdqdqdq <jovavalentino6@gmail.com>
2024-08-28 16:11:04 +0700

    Create AutoWin.lua

---
+++ AutoWin.lua
@@ -0,0 +1,25 @@
+-- Demon Hub🔥
+
+wait(1.2)
+
+game.StarterGui:SetCore("SendNotification", {
+
+Title = "Credits"; -- the title (ofc)
+
+Text = "Made by Demon/Jova,"; -- what the text says (ofc)
+
+Icon = ""; -- the image if u want.
+
+Duration = 5; -- how long the notification should in secounds
+
+})
+
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local Window = Library:NewWindow("DemonHub")
+local Section = Window:NewSection("Build a boat")
+
+Section:CreateToggle("Auto Win", function(value)
+loadstring(game:HttpGet("https://pastebin.com/raw/qqWZv6S7"))()
+print(value)
+end)
