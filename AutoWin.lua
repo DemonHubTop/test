cd53dfbee13bada7ce842b456560b1f3c67fabec
fjjjjjjjjjjjjjjjjjjjjqwdqugdqugqdqnqsiqugdqdqdqdqdq <jovavalentino6@gmail.com>
2024-08-28 16:15:05 +0700

    Update AutoWin.lua

--- AutoWin.lua
+++ AutoWin.lua
@@ -17,10 +17,10 @@ Duration = 5; -- how long the notification should in secounds
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
 local Window = Library:NewWindow("DemonHub")
-local Section = Window:NewSection("Made by Demon")
+local Section = Window:NewSection("Build a boat")
 
-Section:CreateToggle("Auto Parry", function(value)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/archangelmich/bladeball/main/beta.lua"))()
+Section:CreateToggle("Auto Win", function(value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DemonHubTop/test/main/autochest.lua"))()
 print(value)
 end)
 
